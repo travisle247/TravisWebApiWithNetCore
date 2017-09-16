96b3043bdf70a45f44b0c79468a3c8cc474945d0
travisle247 <travisle247@hotmail.com>
2017-09-16 10:30:46 -0500

    Check in

--- TravisWebApiWithAspCore/Controllers/PointsOfInterestController.cs
+++ TravisWebApiWithAspCore/Controllers/PointsOfInterestController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using TravisWebApiWithAspCore.Dao;
+using TravisWebApiWithAspCore.Models;
 
 namespace TravisWebApiWithAspCore.Controllers
 {
@@ -23,7 +24,7 @@ namespace TravisWebApiWithAspCore.Controllers
             return Ok(city.PointsOfInterest);
         }
 
-        [HttpGet("{cityId}/pointsofinterest/{id}")]
+        [HttpGet("{cityId}/pointsofinterest/{id}",Name ="GetPointOfInterest")]
         public IActionResult GetPointsOfInterest(int cityId, int id)
         {
             var city = CitiesDao.Current.Cities.FirstOrDefault(x => x.Id == cityId);
@@ -37,6 +38,35 @@ namespace TravisWebApiWithAspCore.Controllers
                 return NotFound();
             }
             return Ok(pointOfInterest);
+        }
+
+        [HttpPost("{cityId}/pointsofinterest")]
+        public IActionResult CreatePointOfInterest(int cityId, [FromBody] PointOfInterestCreationDto pointOfInterest)
+        {
+            if(pointOfInterest==null)
+            {
+                return BadRequest();
+            }
+
+            var city = CitiesDao.Current.Cities.FirstOrDefault(x => x.Id == cityId);
+
+            if(city==null)
+            {
+                return NotFound();
+            }
+
+            var maxPointOfInterestId = CitiesDao.Current.Cities.SelectMany(c => c.PointsOfInterest).Max(p => p.Id);
+
+            var finalPointOfInterest = new PointsOfInterest()
+            {
+                Id = ++maxPointOfInterestId,
+                Name = pointOfInterest.Name,
+                Description = pointOfInterest.Description
+            };
+
+            city.PointsOfInterest.Add(finalPointOfInterest);
+
+            return CreatedAtRoute("GetPointOfInterest", new { cityId = cityId, id = finalPointOfInterest.Id }, finalPointOfInterest );
         }
     }
 }
---
+++ TravisWebApiWithAspCore/Models/PointOfInterestCreationDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace TravisWebApiWithAspCore.Models
+{
+    public class PointOfInterestCreationDto
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+    }
+}
